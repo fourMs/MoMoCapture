7fd54ab50cd884fc11a9088269b8e93e748c6167
Pedro Pablo Lucas Bravo <pedroplu@uio.no>
2024-03-24 23:32:04 +0100

    Increase app version, android SDK, and fix configurations for teh recent Play Store release under API 34

--- publish1.sh
+++ publish1.sh
@@ -1,3 +1,4 @@
+#Go to the new way to build ***
 rm momocapture.apk
 ionic cordova build android --prod --release
 "C:/Program Files/Java/jdk1.8.0_241/bin/jarsigner.exe" -tsa http://timestamp.digicert.com -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../momocapture.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk alias_name #execute in cmd, not in power shell
@@ -9,9 +10,12 @@ ionic cordova build android
 "C:/Program Files/Java/jdk1.8.0_241/bin/jarsigner.exe" -tsa http://timestamp.digicert.com -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../momocapture.keystore platforms/android/app/build/outputs/bundle/release/app.aab alias_name
 "D:/Android/Sdk/build-tools/31.0.0/zipalign.exe" -v 4 platforms/android/app/build/outputs/bundle/release/app.aab momocapture.aab
 
-#Use this new way, the other ones above are deprecated
+#***Use this new way, the other ones above are deprecated
+#This line can executed in power shell or terminal (Win11)
 rm momocapture.apk
+#Execute next lines in cmd (NOT Power Shell) and open it in the location of this repo
 ionic cordova build android --prod --release
-#Execute next lines in cmd and open it in teh location of this repo
-"D:/Android/Sdk/build-tools/31.0.0/zipalign.exe" -f 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk momocapture.apk
-"D:/Android/Sdk/build-tools/31.0.0/apksigner.bat" sign --ks ../momocapture.keystore --ks-pass file:../momocapture_pass.txt --v1-signing-enabled true --v2-signing-enabled true momocapture.apk
+# in [YOUR PATH HERE] put the right path for the Android SDK
+"[YOUR PATH HERE]/Android/Sdk/build-tools/34.0.0/zipalign.exe" -f 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk momocapture.apk
+
+"[YOUR PATH HERE]/Android/Sdk/build-tools/34.0.0/apksigner.bat" sign --ks ../momocapture.keystore --ks-pass file:../momocapture_pass.txt --v1-signing-enabled true --v2-signing-enabled true momocapture.apk
